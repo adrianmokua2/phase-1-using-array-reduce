50ec728ebb9694b15b6a879b9f82819253ae6e70
Adrian <adrian.mokua@icloud.com>
2023-09-26 15:16:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = batteryBatches.reduce((acc, batch) => acc + batch, 0);
 
+  
 // Code your solution here
+describe('reducer', function() {
+    describe('batteries', function() {
+      it('should have a `totalBatteries` variable', function() {
+        expect(totalBatteries).to.exist;
+      });
+  
+      it('should have a number as a result', function() {
+        expect(totalBatteries).to.be.a('number');
+      });
+  
+      it('should have made the sum of all the assembled batteries', function() {
+        expect(totalBatteries).to.eql(31);
+      });
+    });
+  });
+  
+
+  
+  
